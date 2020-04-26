c0ea92515e49ceb182ae49b21edf55009f7bfd10
anthonya91 <anthonyali591@gmail.com>
2020-04-26 15:36:04 -0400

    webscraping with comments and got rid of the fullscreen and dataframe

--- indeed_webscrape.py
+++ indeed_webscrape.py
@@ -4,23 +4,27 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import csv
 
+## Create a .csv file to import data from the scraping to un utf-8 format 
 csv_file = open('indeed_test.csv', 'w', encoding='utf-8',newline='')
 writer = csv.writer(csv_file)
 
+
+## create a for loop to go through the range of pages. Indeed website pages increases by 10 per pages,
+## so setting the increment to 10. This should go through 500 pages. 
 for i in range(0,5000,10):
     try:
-
+## Using Selenium requires the webdriver directory as well as the URL for the website. Appended "+str(i)" so that it increases. 
 
         driver = webdriver.Chrome(r'C:\Users\Anthony\Desktop\chromedriver.exe')
         URL = "https://www.indeed.com/jobs?q=Covid+Jobs&start="+str(i)
         driver.get("https://www.indeed.com/jobs?q=Covid+Jobs&start="+str(i))
-        #driver.fullscreen_window()
+
 
         soup = BeautifulSoup(urllib.request.urlopen(URL).read(), 'html.parser')
 
         results = soup.find_all('div', attrs={'data-tn-component': 'organicJob'})
-
-        data = pd.DataFrame(columns=["Title","Company","Location","Salary","Summary","Requirements"])
+## initialize Beautiful soup to go through the all of the jobs - results and scrape the title, company, location, salary,
+## summary, and requirements from each job.
 
         for x in results:
 
@@ -55,7 +59,8 @@ for i in range(0,5000,10):
                 if requirement:
                     requirement = requirement.text.strip()
                     print('requirement:', requirement)
-
+## adding the text from the scrape for each category to the empty dictionary created "indeed_dict" under their Category
+## also added print statement in order to see which page the scrape was currently on, to monitor it through the process.
            
                 indeed_dict['Title'] = title
                 indeed_dict['Company'] = company
